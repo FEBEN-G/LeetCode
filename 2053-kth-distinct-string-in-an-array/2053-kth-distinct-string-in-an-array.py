b1822b0e74b3127f88b00b882ec15ba9d903e6c3
FEBEN-G <febengetachew580@gmail.com>
2024-08-06 23:10:54 +0300

    Time: 73 ms (32.13%), Space: 16.7 MB (78.56%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        return next((s for i, s in enumerate((s for s, c in Counter(arr).items() if c == 1), 1) if i == k), '')
+        
